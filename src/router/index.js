9883a9051c1fa09d17eca291016de424aae06391
weizhan <675398552@qq.com>
2018-07-28 15:17:43 +0800

    route

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,13 @@ import Message from "../pages/message/message"
 import Send from "../pages/send/send"
 export default[
     
-    { path:'/home',name:'主页',component:Home,children:[
+    { path:'/',name:'主页',component:Home,children:[
         { path:'/blog',name:'文章',component:Blog},
         { path:'/blog/:id',name:'文章详情',component:Detail},
         { path:'/info',name:'个人',component:Info},
         { path:'/message',name:'留言',component:Message},
         { path:'/write',name:'写博客',component:Send}
-    ],redirect:"/blog"
+        ],redirect:"/blog"
     },
-    {path:'*',redirect:'/index'}
+    {path:'*',redirect:'/blog'}
 ] 
